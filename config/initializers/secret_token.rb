6c3a744e03d8f6f19fcd4e459eff92210954ecda
sammyrulez <sammyrulez@gmail.com>
2011-02-01 10:36:06 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterBlast::Application.config.secret_token = '547c44f4f65a99c88dc7a117a43da5b556401e87b5f2f54c98ba18a8662252a82a00c6ddfb81ef7725902c9fb02f680e76a03767f40c6523e7b2fbb02c0f8aeb'
